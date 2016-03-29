2963470369b509de88914fa4654145777c2c7426
Germán Prado <gerpra@gmail.com>
2016-03-29 10:39:40 +0200

    Repositorio segundo de la práctica 4 de Entornos de desarrollo.

--- src/dam_ed04_actividad/Main.java
+++ src/dam_ed04_actividad/Main.java
@@ -11,7 +11,8 @@ public class Main {
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
-
+        
+        //comprobamos el saldo de Antonio López
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
